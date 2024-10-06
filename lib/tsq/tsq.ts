be92eb84ce7c8b6cdc90ce9913c7c517cef2ec3f
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2024-10-06 13:54:13 -0700

    Update tsq.ts

--- lib/tsq/tsq.ts
+++ lib/tsq/tsq.ts
@@ -1,27 +1,10 @@
 // TODO: Implement tsq.
 
-import type { Capture, Parser, Tree } from "#/lib/tree-sitter/mod.ts";
+import type { Capture, Tree } from "#/lib/tree-sitter/mod.ts";
 
-export function tsq(tree: Tree, query: TreeSitterQuery): TreeSitterQueryResult {
-  console.log(JSON.stringify(tree, null, 2));
-  return tree?.rootNode.query(compileTreeSitterQuery(query));
-}
-
-export function compileTreeSitterQuery(_query: TreeSitterQuery): string {
-  return "";
-}
-
-export interface TreeSitterQuery {
-  nodes: TreeSitterQueryNode[];
-}
-
-export interface TreeSitterQueryNode {
-  fields?: TreeSitterQueryField[];
-  nodes?: TreeSitterQueryNode[];
-}
-
-export interface TreeSitterQueryField {
-  // TODO: Implement.
+export function tsq(tree: Tree, query: string): TreeSitterQueryResult {
+  // TODO: Test out tsq.
+  return tree?.rootNode.query(query);
 }
 
 export interface TreeSitterQueryResult {
